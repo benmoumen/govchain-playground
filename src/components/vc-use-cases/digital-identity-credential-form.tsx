9ce6a4124979450c28907a2b374cc78ec4d82809
Moulay Mehdi BENMOUMEN <benmoumen@gmail.com>
2025-01-02 17:22:17 +0300

    Enhance DigitalIdentityCredentialForm with date handling improvements and random data generation

--- src/components/vc-use-cases/digital-identity-credential-form.tsx
+++ src/components/vc-use-cases/digital-identity-credential-form.tsx
@@ -1,56 +1,82 @@
 import React from "react";
+import { z } from "zod";
+
 import CredentialForm, {
   VCFormFieldEnum,
   type VCFormFieldDefinition,
-} from "../credential-form";
-import { z } from "zod";
+} from "@/components/credential-form";
+import { formatDateToYYYYMMDD } from "@/lib/utils";
 
 const MaritalStatusEnum = z.enum(["Single", "Married", "Divorced", "Widowed"]);
 const SexEnum = z.enum(["Male", "Female"]);
-const NationalityEnum = z.enum(["Salvadorian"]);
 
 const DigitalIdentityCredentialForm: React.FC = () => {
   const digitalIdentitySchema = z.object({
     "Full Name": z
       .string()
       .min(2, { message: "Full name must be at least 2 characters." }),
-    "Document Number": z
-      .string()
-      .min(1, { message: "Document number is required." }),
-    "Date of Birth": z.date({ required_error: "Date of Birth is required." }),
-    "Place of Birth": z
-      .string()
-      .min(1, { message: "Place of Birth is required." }),
+    "Document Number": z.string({
+      required_error: "Document number is required.",
+    }),
+    "Date of Birth": z
+      .string({
+        required_error: "Date of Birth is required.",
+      })
+      .transform((str) => new Date(str)),
+    "Place of Birth": z.string({
+      required_error: "Place of Birth is required.",
+    }),
     Sex: SexEnum.optional(),
-    Nationality: NationalityEnum.optional(),
     "Marital Status": MaritalStatusEnum.optional(),
-    Address: z.string().min(1, { message: "Address is required." }),
+    Address: z.string({ required_error: "Address is required." }),
     Municipality: z.string().optional(),
     Department: z.string().optional(),
-    "Date of Issue": z.date({ required_error: "Date of Issue is required." }),
-    "Date of Expiry": z.date({ required_error: "Date of Expiry is required." }),
+    "Date of Issue": z
+      .string({
+        required_error: "Date of Issue is required.",
+      })
+      .transform((str) => new Date(str)),
+    "Date of Expiry": z
+      .string({
+        required_error: "Date of Expiry is required.",
+      })
+      .transform((str) => new Date(str)),
   });
 
   const today = new Date();
   const expiryDate = new Date();
-  const expirationYear = today.getFullYear() + 10;
-  expiryDate.setFullYear(expirationYear);
+  expiryDate.setFullYear(today.getFullYear() + 10);
+  const birthDate = new Date();
+  birthDate.setFullYear(today.getFullYear() - 20);
 
-  const defaultValues = {
+  const generateRandomDUIDocumentNumber = () => {
+    return Math.floor(100000000 + Math.random() * 900000000).toString();
+  };
+
+  const getRandomElement = (arr: string[]) =>
+    arr[Math.floor(Math.random() * arr.length)];
+
+  const defaultValues: { [key: string]: string | undefined } = {
     "Full Name": "Juan Carlos Martinez",
-    "Document Number": "123456789",
-    "Date of Birth": "1990-06-20",
+    "Document Number": generateRandomDUIDocumentNumber(),
+    "Date of Birth": formatDateToYYYYMMDD(
+      new Date(
+        birthDate.setFullYear(
+          today.getFullYear() - Math.floor(Math.random() * 50 + 18)
+        )
+      )
+    ),
     "Place of Birth": "San Salvador, El Salvador",
-    Sex: "Male",
+    Sex: getRandomElement(SexEnum.options),
     Nationality: "Salvadoran",
-    "Marital Status": "Single",
+    "Marital Status": getRandomElement(MaritalStatusEnum.options.slice(0, 2)),
     Address: "Calle Principal #123, San Salvador, El Salvador",
     Municipality: "San Salvador",
     Department: "San Salvador",
     Photograph: "base64-encoded-photograph",
     Signature: "base64-encoded-signature",
-    "Date of Issue": today.toISOString().split("T")[0],
-    "Date of Expiry": expiryDate.toISOString().split("T")[0],
+    "Date of Issue": formatDateToYYYYMMDD(today),
+    "Date of Expiry": formatDateToYYYYMMDD(expiryDate),
   };
 
   const formFields: VCFormFieldDefinition[] = [
@@ -66,6 +92,7 @@ const DigitalIdentityCredentialForm: React.FC = () => {
       name: "Date of Birth",
       label: "Date of Birth",
       type: VCFormFieldEnum.Date,
+      toDate: birthDate,
       hidden: true,
     },
     {
@@ -79,13 +106,6 @@ const DigitalIdentityCredentialForm: React.FC = () => {
       label: "Sex",
       type: VCFormFieldEnum.Enum,
       options: SexEnum.options,
-    },
-    {
-      name: "Nationality",
-      label: "Nationality",
-      type: VCFormFieldEnum.Enum,
-      options: NationalityEnum.options,
-      hidden: true,
     },
     {
       name: "Marital Status",
