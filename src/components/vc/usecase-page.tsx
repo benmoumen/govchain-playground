7ceaf0bf5c9c28138e855ce6bddd2bd6ea143c19
Moulay Mehdi BENMOUMEN <benmoumen@gmail.com>
2025-01-15 16:31:34 +0300

    feat: enhance use case page with connection and request steps, add connection polling and QR code invitation

--- src/components/vc/usecase-page.tsx
+++ src/components/vc/usecase-page.tsx
@@ -1,55 +1,150 @@
 "use client";
+import { AppDownloadButtons } from "@/components/ui/app-download-buttons";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import CredentialForm from "@/components/vc/credential-form";
+import VCConnectionCard from "@/components/vc/vc-connection-card";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React, { Suspense } from "react";
-import DigitalIdentityCredentialForm from "./digital-identity/form";
-import VCConnectionCard from "./vc-connection-card";
+  getTenantByCase,
+  getUseCaseForm,
+  getUseCaseMetadata,
+} from "@/config/vc";
+import type { ConnRecord } from "@/types/vc/acapyApi/acapyInterface";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { AnimatedTabs } from "../ui/animated-tabs";
 
 interface UseCasePageProps {
   useCase: string;
 }
 
 const UseCasePage: React.FC<UseCasePageProps> = ({ useCase }) => {
+  const { credentialName, src } = getUseCaseMetadata(useCase);
+  const issuer = getTenantByCase(useCase);
+  const { schema, fields, defaultValues } = getUseCaseForm(useCase);
+  enum steps {
+    CONNECT,
+    REQUEST,
+    END,
+  }
+  const [activeStep, setActiveStep] = useState<number>(steps.CONNECT);
+  const [connection] = useState<ConnRecord | null>(null);
+
+  const PageFooter: React.FC = () => (
+    <>
+      <div className="button-title text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+        Get your Digital Wallet app ready!
+      </div>
+      <AppDownloadButtons />
+    </>
+  );
+
+  const StepConnect: React.FC = () => (
+    <Card className="overflow-hidden">
+      <CardContent className="grid p-0 md:grid-cols-2">
+        <div className="relative p-6 md:p-8">
+          <div className="flex flex-col gap-6 items-center">
+            <div className="flex flex-col items-center text-center">
+              <h1 className="text-2xl font-bold">Connect to the issuer</h1>
+            </div>
+
+            <VCConnectionCard
+              useCase={useCase}
+              issuer={issuer}
+              credentialName={credentialName}
+            />
+          </div>
+        </div>
+        <div className="relative hidden bg-muted md:block">
+          {connection && (
+            <div className="absolute inset-0 z-10 flex items-center justify-center">
+              <Button
+                effect={"expandIcon"}
+                icon={ChevronRight}
+                iconPlacement="left"
+                variant={"secondary"}
+                onClick={() => setActiveStep(steps.REQUEST)}
+              >
+                {" "}
+                Request your {credentialName}
+              </Button>
+            </div>
+          )}
+
+          <Image
+            src={src}
+            sizes="(min-width: 768px) 50vw, 100vw"
+            alt="Image"
+            fill
+            style={{ objectFit: "cover" }}
+            className={`absolute inset-0 dark:brightness-[0.8] transition-all duration-300 ${
+              connection ? "blur-sm" : ""
+            }`}
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+
+  const StepRequest: React.FC = () => (
+    <Card className="overflow-hidden">
+      <CardContent className="grid p-0 md:grid-cols-3">
+        <div className="relative col-span-1 hidden bg-muted md:block">
+          <Image
+            src={src}
+            sizes="(min-width: 768px) 50vw, 100vw"
+            alt="Image"
+            fill
+            style={{ objectFit: "cover" }}
+            className={`absolute inset-0 dark:brightness-[0.8] transition-all duration-300 ${
+              connection ? "blur-sm" : ""
+            }`}
+          />
+        </div>
+        <div className="relative col-span-2 p-6 md:p-8">
+          <div className="flex flex-col gap-6 items-center">
+            <h1 className="text-2xl font-bold">Request a credential</h1>
+            <CredentialForm
+              schema={schema}
+              formFields={fields}
+              defaultValues={defaultValues}
+            />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+
+  const tabs = [
+    {
+      title: "Connect",
+      value: "connect",
+      content: <StepConnect />,
+    },
+    {
+      title: "Request",
+      value: "request",
+      content: <StepRequest />,
+    },
+  ];
+
   return (
-    <Tabs defaultValue="connect" className="w-full">
-      <TabsList>
-        <TabsTrigger value="connect">Connect with the Issuer</TabsTrigger>
-        <TabsTrigger value="data">Credential Data</TabsTrigger>
-        <TabsTrigger value="request" disabled>
-          Request Credential
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="connect">
-        <Card>
-          <CardHeader>
-            <CardTitle>Scan the QR code using your Wallet</CardTitle>
-            <CardDescription>
-              When you connect with the issuer, you will be able to obtain
-              credentials and send inquiries.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <Suspense fallback={<p>Loading...</p>}>
-              <VCConnectionCard useCase={useCase} />
-            </Suspense>
-          </CardContent>
-          <CardFooter>Footer</CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="data">
-        <DigitalIdentityCredentialForm />
-      </TabsContent>
-      <TabsContent value="request">
-        <p>Request Credential content goes here.</p>
-      </TabsContent>
-    </Tabs>
+    <div className="flex flex-col gap-6 items-center">
+      <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start">
+        <AnimatedTabs
+          tabs={tabs}
+          currentStep={activeStep}
+          onStepChange={function (step: number): void {
+            console.info("onStepChange", step);
+          }}
+        />
+      </div>
+      <Button onClick={() => setActiveStep(0)}>Connect</Button>
+      <Button onClick={() => setActiveStep(1)}>Request</Button>
+
+      <PageFooter />
+    </div>
   );
 };
 
--- src/components/vc/vc-connection-card.tsx
+++ src/components/vc/vc-connection-card.tsx
@@ -1,74 +1,158 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import useConnection from "@/hooks/vc/use-connection";
-import { Loader2, RefreshCcwDotIcon, RefreshCw } from "lucide-react";
+import type { VCIssuer } from "@/types/vc";
+import type { ConnRecord } from "@/types/vc/acapyApi/acapyInterface";
+import { Loader2, RefreshCcw, Waypoints } from "lucide-react";
+import { motion } from "motion/react";
+import { useTheme } from "next-themes";
 import { QRCodeSVG } from "qrcode.react";
 import { useEffect } from "react";
+import { toast } from "sonner";
+import { BackgroundLines } from "../ui/background-lines";
 import { Button } from "../ui/button";
+import { MessageLoading } from "../ui/message-loading";
+import { ShineBorder } from "../ui/shine-border";
 
 interface VCConnectionCardProps {
   useCase: string;
+  issuer: VCIssuer;
+  credentialName: string;
+  setConnection?: (conn: ConnRecord) => void;
 }
 
-const VCConnectionCard: React.FC<VCConnectionCardProps> = ({ useCase }) => {
+const VCConnectionCard: React.FC<VCConnectionCardProps> = ({
+  useCase,
+  issuer,
+  credentialName,
+  setConnection,
+}) => {
+  const { resolvedTheme } = useTheme();
   const {
-    isActiveConnection,
-    error,
+    activeConnection,
     initiateConnection,
-    connectionId,
-    invitationUrl,
     generatingInvitation,
+    invitationUrl,
+    isPolling,
+    error,
   } = useConnection(useCase);
 
   useEffect(() => {
-    if (isActiveConnection) {
+    if (activeConnection) {
+      //setConnection(activeConnection);
       console.log("Connection has been made successfully!");
     }
-  }, [isActiveConnection]);
+  }, [activeConnection, setConnection]);
 
   useEffect(() => {
-    if (!connectionId) {
-      initiateConnection();
-    }
+    console.log("Calling initiate connection");
+    initiateConnection();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (error) {
-    return <p>Error loading connection data.</p>;
+  const InvitationQRCode: React.FC<{ url: string; isDark: boolean }> = ({
+    url,
+    isDark,
+  }) => (
+    <div className="flex flex-col items-center gap-4">
+      <p className="text-center text-balance text-muted-foreground">
+        You are invited to connect with <strong>{issuer.name}</strong>. Scan to
+        proceed.
+      </p>
+
+      <ShineBorder
+        className="relative flex h-[280px] w-[280px] flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"
+        color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
+      >
+        <QRCodeSVG
+          title="Scan the QR code to connect"
+          value={url}
+          size={280}
+          marginSize={3}
+          fgColor={isDark ? "#fff" : "#000"}
+          bgColor={isDark ? "#000" : "#fff"}
+        />
+      </ShineBorder>
+    </div>
+  );
+
+  const ConnectionSuccessAlert: React.FC = () => (
+    <BackgroundLines className="items-center justify-center w-full flex-col px-4">
+      <Alert variant={"success"}>
+        <Waypoints className="h-4 w-4" />
+        <AlertTitle>
+          You can now request your <strong>{credentialName}</strong>.
+        </AlertTitle>
+        <AlertDescription>
+          Your wallet is connected with <strong>{issuer.name}</strong>.
+        </AlertDescription>
+      </Alert>
+    </BackgroundLines>
+  );
+
+  const NewInvitationButton: React.FC<{ loading: boolean }> = ({ loading }) => (
+    <Button
+      onClick={() => initiateConnection(true)}
+      variant={"link"}
+      size={"sm"}
+      disabled={loading}
+    >
+      {loading ? (
+        <>
+          <Loader2 className="animate-spin" />
+          Creating invitation...
+        </>
+      ) : (
+        <>
+          <RefreshCcw />
+          Request a new invitation
+        </>
+      )}
+    </Button>
+  );
+
+  const ConnectionPolling: React.FC = () => (
+    <motion.div
+      initial={{
+        opacity: 1,
+      }}
+      animate={{
+        opacity: [1, 0.2, 1],
+      }}
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+      }}
+      className="flex justify-center items-center gap-2 p-2"
+    >
+      <MessageLoading />
+      <span className="text-muted-foreground text-sm">
+        Checking connection status...
+      </span>
+    </motion.div>
+  );
+
+  if (error && !activeConnection) {
+    toast.error("Error while fetching your connection state");
   }
 
   return (
-    <div>
-      <h1>VC Connection Card</h1>
-      <p>Use Case: {useCase}</p>
-      {isActiveConnection ? (
-        <>
-          <p>Connection has been made successfully!</p>
-          <Button
-            variant={"destructive"}
-            onClick={() => initiateConnection(true)}
-          >
-            <RefreshCw />
-            Start over
-          </Button>
-        </>
-      ) : invitationUrl ? (
-        <div>
-          <p>Scan the QR code to connect:</p>
-          <QRCodeSVG value={invitationUrl} size={220} />
-        </div>
+    <div className="min-w-[300px] flex flex-col justify-center items-center gap-2">
+      {activeConnection ? (
+        <ConnectionSuccessAlert />
       ) : (
-        <Button
-          onClick={() => initiateConnection(true)}
-          disabled={generatingInvitation}
-        >
-          {generatingInvitation ? (
-            <Loader2 className="animate-spin" />
-          ) : (
-            <RefreshCcwDotIcon />
-          )}
-          {generatingInvitation
-            ? "Generating invitation..."
-            : "Request a new invitation"}
-        </Button>
+        invitationUrl && (
+          <InvitationQRCode
+            url={invitationUrl}
+            isDark={resolvedTheme === "dark"}
+          />
+        )
+      )}
+
+      {isPolling && (
+        <>
+          <ConnectionPolling />
+          <NewInvitationButton loading={generatingInvitation} />
+        </>
       )}
     </div>
   );
