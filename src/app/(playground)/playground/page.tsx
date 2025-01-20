b33c280a8487bd8a80a10ab3323239984fe9abab
Moulay Mehdi BENMOUMEN <benmoumen@gmail.com>
2025-01-21 00:22:02 +0300

    feat: update SplineScene source

--- src/app/(playground)/playground/page.tsx
+++ src/app/(playground)/playground/page.tsx
@@ -1,10 +1,17 @@
+import { Card } from "@/components/ui/card";
 import { SplineScene } from "@/components/ui/spline-scene";
 
 export default function Page() {
   return (
-    <SplineScene
-      scene="https://prod.spline.design/zb6Q9Cc27u0gHhSn/scene.splinecode"
-      className="w-full h-full"
-    ></SplineScene>
+    <div className="w-full h-full p-5 flex items-center justify-center">
+      <Card className="w-full h-[600px] bg-black/[0.96] relative overflow-hidden">
+        <div className="flex h-full">
+          <SplineScene
+            scene="https://draft.spline.design/6P4G2rL-QNk8ORGq/scene.splinecode"
+            className="w-full h-full"
+          ></SplineScene>
+        </div>
+      </Card>
+    </div>
   );
 }
