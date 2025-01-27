a7eba3bafa4fd5804d61f9b854606722d50ebe41
Moulay Mehdi BENMOUMEN <benmoumen@gmail.com>
2025-01-27 11:46:44 +0300

    feat: add Docker support with Dockerfile and .dockerignore

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [{ hostname: "images.unsplash.com" }],
   },
   reactStrictMode: false,
+  output: "standalone",
 };
 
 export default nextConfig;
