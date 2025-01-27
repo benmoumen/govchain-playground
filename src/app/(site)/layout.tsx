ee159404180b9875712dd83210805e7e682630b3
Moulay Mehdi BENMOUMEN <benmoumen@gmail.com>
2025-01-27 14:16:53 +0300

    fix: make SiteHeader reusable in different layouts by passing children prop

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { SiteHeader } from "@/components/site-header";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { Button } from "@/components/ui/button";
 
 interface SiteLayoutProps {
   children: React.ReactNode;
@@ -11,7 +12,14 @@ export default function SiteLayout({ children }: SiteLayoutProps) {
   return (
     <div className="relative flex min-h-screen flex-col">
       <div className="relative flex min-h-screen flex-col">
-        <SiteHeader />
+        <SiteHeader>
+          <span className="text-xs text-muted-foreground/80">
+            <span className="hidden md:inline-block">Made by</span>
+            <Button variant={"link"} size={"sm"} effect={"hoverUnderline"}>
+              UNCTAD Digital Government
+            </Button>
+          </span>
+        </SiteHeader>
         {children}
       </div>
       <TailwindIndicator />
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,13 +1,18 @@
 import { Icons } from "@/components/icons";
 import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 
-export function SiteHeader() {
+import { ReactNode } from "react";
+
+interface SiteHeaderProps {
+  children: ReactNode;
+}
+
+export function SiteHeader({ children }: SiteHeaderProps) {
   return (
     <header className="sticky top-0 z-[60] py-2 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container z-10 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container z-10 flex h-16 items-center gap-4 space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-3">
             <Icons.logo className="h-8" />
@@ -16,14 +21,9 @@ export function SiteHeader() {
             </span>
           </Link>
         </div>
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <span className="text-xs text-muted-foreground/80">
-              <span className="hidden md:inline-block">Made by</span>
-              <Button variant={"link"} size={"sm"} effect={"hoverUnderline"}>
-                UNCTAD Digital Government
-              </Button>
-            </span>
+        <div className="flex flex-1 items-center justify-end">
+          <nav className="flex items-center gap-6">
+            {children}
             <ThemeToggle />
           </nav>
         </div>
