753c257189dfbc10ffb0c1894783dd0251985bd3
Moulay Mehdi BENMOUMEN <benmoumen@gmail.com>
2025-12-10 15:06:13 +0300

    feat: add Rwanda chat widget configurations

--- src/components/embedded-scripts.tsx
+++ src/components/embedded-scripts.tsx
@@ -1,7 +1,61 @@
 export function EmbeddedScripts() {
   return (
     <>
-      {/* <Script
+      {/* --- Rwanda ---
+      <script
+        // Core Embed Settings
+        data-embed-id="e1b3b563-d370-46e3-9abd-4f46f0ed20c9"
+        data-base-api-url="https://chat.tutorial.eregistrations.org/api/embed"
+        src="https://chat.tutorial.eregistrations.org/embed/anythingllm-chat-widget.min.js"
+        // Customizations
+        data-greeting="Welcome! I'm the AI assistant for Rwanda Trade Information Portal. How can I assist you today?"
+        data-button-color="#6766fc"
+        data-chat-icon="chatBubble"
+        data-user-bg-color="#3d83d5"
+        data-assistant-bg-color="#f0faff"
+        data-brand-image-url="https://rwandatrade.rw/PublicContent/images/Coat_of_arms_of_Rwanda.svg_7.png"
+        data-assistant-name="Trade Portal AI Assistant"
+        data-assistant-icon="https://rwandatrade.rw/PublicContent/images/Coat_of_arms_of_Rwanda.svg_7.png"
+        data-position="bottom-left"
+        data-window-height="600px"
+        data-window-width="400px"
+        //data-text-size="14px"
+        data-no-sponsor="true" // Hides the sponsor footer
+        data-default-messages="How do I export Avocado from Rwanda?, What clearance steps are required for motor vehicles imports?"
+        // Behavior Overrides
+        data-open-on-load="on"
+        data-show-thoughts="false"
+        data-support-email="chrispicton.shyaka@rdb.rw"
+      ></script>
+
+      <script
+        // Core Embed Settings
+        data-embed-id="6bb42c60-a35a-42cd-88e4-1daf5083231c"
+        data-base-api-url="https://chat.tutorial.eregistrations.org/api/embed"
+        src="https://chat.tutorial.eregistrations.org/embed/anythingllm-chat-widget.min.js"
+        // Customizations
+        data-greeting="Welcome! I'm the AI assistant from the Rwanda Development Board. How can I assist you today?"
+        data-button-color="#6766fc"
+        data-chat-icon="chatBubble"
+        data-user-bg-color="#3d83d5"
+        data-assistant-bg-color="#f0faff"
+        data-brand-image-url="https://rwandatrade.rw/PublicContent/images/Coat_of_arms_of_Rwanda.svg_7.png"
+        data-assistant-name="AI Assistant for Business Procedures"
+        data-assistant-icon="https://rwandatrade.rw/PublicContent/images/Coat_of_arms_of_Rwanda.svg_7.png"
+        data-position="bottom-left"
+        data-window-height="600px"
+        data-window-width="400px"
+        //data-text-size="14px"
+        data-no-sponsor="true" // Hides the sponsor footer
+        data-default-messages="How do I register as an individual trader?, How to obtain a business visa?"
+        // Behavior Overrides
+        data-open-on-load="on"
+        data-show-thoughts="false"
+        data-support-email="chrispicton.shyaka@rdb.rw"
+      ></script>
+
+      --- Tanzania ---
+      <Script
         // Core Embed Settings
         data-embed-id="cf451081-13a8-4d9c-85d8-2a5005990beb"
         data-base-api-url="https://chat.tutorial.eregistrations.org/api/embed"
